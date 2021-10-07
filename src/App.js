19139c30422ad30763891f36b47f2d603c3647a8
amin naseralddini <s.amin.naseralddini@gmail.com>
2021-10-07 19:10:04 +0330

    create select option for group

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import SelectComponent from './Components/SelectComponent';
 
 function App() {
   return (
     <div className="App">
-
+      <h1>طبقه بندی محصولات فروشگاه</h1>
+      <SelectComponent />
+      <ToastContainer position="top-left" rtl={true} />
     </div>
   );
 }
---
+++ src/Components/SelectComponent.jsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import Select from 'react-select';
+import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { useRef } from 'react';
+
+const options = [
+	{ value: 'dairy', label: 'Dairy' },
+	{ value: 'nuts', label: 'Nuts' },
+	{ value: 'fruitage', label: 'Fruitage' },
+];
+
+
+const SelectComponent = () => {
+	const [addToGroup, setAddToGroup] = useState(options);
+	const [getValue, setGetValue] = useState({
+		value: '',
+		label: ''
+	});
+
+
+	const inputRef = useRef();
+
+	// Save & Show input value in selectOption when click button
+	const addToSelectHandler = () => {
+		if (inputRef.current.value === "") {
+			toast.warning('لطفا نام گروه را وارد کنید', { theme: "colored" })
+		} else {
+			setAddToGroup([...addToGroup, getValue]);
+			toast.success('گروه جدید اضافه شد', { theme: "colored", fontFamily: "vazir" })
+			inputRef.current.value = '';
+		}
+	}
+
+	// Get value from input & store in getValue useState 
+	const inputHandler = (e) => {
+		setGetValue({ ...getValue, value: e.target.value, label: e.target.value });
+	}
+
+	// Get value from click on select options
+	const selectedValueHandler = (e) => {
+		console.log(e.value);
+	}
+
+	return (
+		<div className="select-icon">
+			<div className="select-elements">
+				<div>
+					<span> نام گروه را انتخاب نمائید </span>
+					<Select
+						options={addToGroup}
+						placeholder="طبقه بندی"
+						onChange={selectedValueHandler}
+					/>
+				</div>
+				<div className="input-add-group">
+					<label>نام گروه جدید</label>
+					<input type="text" name="value" onChange={inputHandler} ref={inputRef} />
+					<button className="btn" onClick={addToSelectHandler}>اضافه به محصولات </button>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default SelectComponent;
